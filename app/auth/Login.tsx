db1bc7845e8debd5ab6973e94b6dfd239a269447
elizabeth.goullaud <egoullaud@gmail.com>
2023-05-10 12:56:08 -0700

    added auth sign in

--- app/auth/Login.tsx
+++ app/auth/Login.tsx
@@ -5,7 +5,10 @@ import { signIn } from "next-auth/react";
 export default function Login() {
   return (
     <li className="list-none">
-      <button className="text-sm" onClick={() => signIn()}>
+      <button
+        className="text-sm bg-gray-700 text-white rounded-xl py-2 px-6 disabled:opacity-25"
+        onClick={() => signIn()}
+      >
         Sign in
       </button>
     </li>
