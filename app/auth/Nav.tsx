e694f688f8679646df645f4aab42277a3694cfa9
elizabeth.goullaud <egoullaud@gmail.com>
2023-05-10 12:59:36 -0700

    logged in  added

--- app/auth/Nav.tsx
+++ app/auth/Nav.tsx
@@ -12,7 +12,8 @@ export default async function Nav() {
         <h1 className="font-bold text-lg">Send it.</h1>
       </Link>
       <ul className="flex items-center gap-6">
-        <Login />
+        {!session?.user && <Login />}
+        {session?.user && <h1>{session.user.name}</h1>}
       </ul>
     </nav>
   );
