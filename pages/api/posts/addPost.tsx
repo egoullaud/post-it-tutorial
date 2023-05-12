40be672c7087baae54db6c9119962cc0a41e3e57
elizabeth.goullaud <egoullaud@gmail.com>
2023-05-12 12:26:01 -0700

    add api comms

--- pages/api/posts/addPost.tsx
+++ pages/api/posts/addPost.tsx
@@ -1,6 +1,43 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]";
+import prisma from "../../../prisma/client";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === "POST") {
+    const session = await getServerSession(req, res, authOptions);
+    if (!session)
+      return res.status(401).json({ message: "Please Sign in to make a post" });
+
+    const title: string = req.body.title;
+
+    //get user
+    const prismaUser = await prisma.user.findUnique({
+      where: { email: session?.user?.email },
+    });
+
+    //check title length
+    if (title.length > 300)
+      return res.status(403).json({ message: "Please write shorter post" });
+    if (!title.length)
+      return res.status(403).json({ message: "Must write something to post" });
+
+    //create post
+    try {
+      const result = await prisma.post.create({
+        data: {
+          title,
+          userId: prismaUser.id,
+        },
+      });
+      res.status(200).json(result);
+    } catch (err) {
+      res
+        .status(403)
+        .json({ message: "Error has occured while making a post" });
+    }
   }
 }
