1f8293db7fc927eb5c3b570a084f94494ae8fb7d
elizabeth.goullaud <egoullaud@gmail.com>
2023-05-12 12:58:52 -0700

    add get posts api call

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
 "use client";
 
 import AddPost from "./components/AddPost";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+
+//fetch all posts
+const allPosts = async () => {
+  const response = await axios.get("/api/posts/getPosts");
+  return response.data;
+};
 
 export default function Home() {
+  const { data, error, isLoading } = useQuery({
+    queryFn: allPosts,
+    queryKey: ["posts"],
+  });
+  if (error) return error;
+  if (isLoading) return "Loading...";
+  console.log(data);
   return (
     <main>
       <AddPost />
